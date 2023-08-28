6c122f388c98fd63d748253d720107dd81cde265
Richardyson <ri_chardyson@hotmail.com>
2023-08-28 12:20:05 -0300

    ok

---
+++ Nyouise/citacoes.js
@@ -0,0 +1,17 @@
+export const databaseCitacoes = {
+    "albert-einstein": [
+        "A imaginação é mais importante que o conhecimento.",
+        "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original.",
+        // Adicione mais citações de Albert Einstein aqui
+    ],
+    "mahatma-gandhi": [
+        "Seja a mudança que você deseja ver no mundo.",
+        "A alegria está na luta, na tentativa, no sofrimento envolvido, não na vitória propriamente dita.",
+        // Adicione mais citações de Mahatma Gandhi aqui
+    ],
+    "martin-luther-king": [
+        "Eu tenho um sonho.",
+        "A escuridão não pode expulsar a escuridão: apenas a luz pode fazer isso.",
+        // Adicione mais citações de Martin Luther King Jr. aqui
+    ]
+};
